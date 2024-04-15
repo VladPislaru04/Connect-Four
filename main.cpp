c15652edf5a66f15b99769f5fbb5f21b72773d7d
Vlad Pislaru <vladpislaru.info@gmail.com>
2024-04-15 11:59:29 +0300

    resolved comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
 
-class Player{
+const char EMPTY_CELL = '#';
+const char ID1 = 'X';
+const char ID2 = 'O';
+
+class Player {
 private:
-	string playerName;
+	std::string playerName;
 	char playerID;
 	bool isActive;
 public:
@@ -12,26 +16,24 @@ public:
         isActive = false;
     }
 
+    Player(std::string name, char ID) {
+        playerName = name;
+        playerID = ID;
+    }
+
     Player(char ID) {
         playerName = "Player" + ID;
         playerID = ID;
     }
 
-    void setPlayerName(const string name) {
-        playerName = name;
-    }
-
-    string getPlayerName() {
+    std::string getPlayerName() {
         return playerName;
-    }
-
-    void setPlayerID(const char ID) {
-        playerID = ID;
     }
 
     char getPlayerID () {
         return playerID;
     }
+
     friend std::ostream& operator<< (std::ostream& out, const Player& player)
     {
         out << player.playerName;
@@ -43,7 +45,7 @@ public:
 class Board {
 private:
     int height, length;
-    vector<string> state;
+    std::vector<std::string> state;
 public:
     Board() {
         height = 6;
@@ -51,7 +53,7 @@ public:
         state.resize(height + 1);
         for (int i = 0; i < height; i++)
             for (int j = 0; j < length; j++)
-                state[i] += '#';
+                state[i] += EMPTY_CELL;
     }
     Board(int new_height, int new_length) {
         height = new_height;
@@ -59,17 +61,7 @@ public:
         state.resize(height);
         for (int i = 0; i < height; i++)
             for (int j = 0; j < length; j++)
-                state[i] += '#';
-    }
-    void displayBoard() {
-        for (int i = 0; i < height; i++) {
-            cout << "|";
-            for (int j = 0; j < length; j++)
-                cout << state[i][j];
-            cout << "|";
-            cout << "\n";
-        }
-        cout << "\n";
+                state[i] += EMPTY_CELL;
     }
     friend std::ostream& operator<< (std::ostream& out, const Board& board)
     {
@@ -87,36 +79,37 @@ public:
         int win = 0;
         for(int i = height - 1; i >= 0; i--)
         {
-            for(int j = length; j >= 0; --j )
+            for(int j = length - 1; j >= 0; j-- )
             {
-                if(i >= 3 && j >= 3 && state[i][j] == activePlayerID && state[i-1][j-1] == activePlayerID &&
-                    state[i-2][j-2] == activePlayerID && state[i-3][j-3] == activePlayerID) {
 
-                    win = 1;
-                }
-
+                /// check horizontal
                 if(j >= 3 && state[i][j] == activePlayerID && state[i][j-1] == activePlayerID &&
                     state[i][j-2] == activePlayerID && state[i][j-3] == activePlayerID) {
 
                     win = 1;
                 }
 
+                /// check vertical
                 if(i >= 3 && state[i][j] == activePlayerID && state[i-1][j] == activePlayerID &&
                     state[i-2][j] == activePlayerID && state[i-3][j] == activePlayerID) {
 
                     win = 1;
                 }
 
+                /// check diagonal going bottom left to top right
+                if(i >= 3 && j >= 3 && state[i][j] == activePlayerID && state[i-1][j-1] == activePlayerID &&
+                    state[i-2][j-2] == activePlayerID && state[i-3][j-3] == activePlayerID) {
+
+                    win = 1;
+                }
+
+                /// check diagonal going bottom right to top left
                 if(i >= 3 && j + 3 < length && state[i][j] == activePlayerID && state[i-1][j+1] == activePlayerID &&
                     state[i-2][j+2] == activePlayerID && state[i-3][j+3] == activePlayerID) {
 
                     win = 1;
                 }
 
-                if (j + 3 < length && state[i][j] == activePlayerID && state[i][j+1] == activePlayerID &&
-                     state[i][j+2] == activePlayerID && state[i][j+3] == activePlayerID) {
-                    win = 1;
-                }
             }
         }
 
@@ -125,36 +118,36 @@ public:
 
     int makeMove(Player activePlayer) {
 
-        int choice, steps = 0;
-        cout << activePlayer.getPlayerName()<< "'s Turn ";
-        cout << "Please enter a number between 1 and " << length << ": ";
-        cin  >> choice;
+        int choice = 0, steps = 0;
+        std::cout << activePlayer.getPlayerName()<< "'s Turn ";
+        std::cout << "Please enter a number between 1 and " << length << ": ";
+        std::cin  >> choice;
         if (choice == 0) {
-            cout << "Wrong format!\n";
+            std::cout << "Wrong format!\n";
             return -1;
         }
-        while(choice < 1 || choice > length || state[0][choice - 1] == 'X' || state[0][choice - 1] == 'O') {
+        while(choice < 1 || choice > length || state[0][choice - 1] == ID1 || state[0][choice - 1] == ID2) {
 
             steps++;
             if (steps > 100) {
-                cout << "Too many wrong tries!\n";
+                std::cout << "Too many wrong tries!\n";
                 return -1;
             }
 
             if (choice < 1 || choice > length) {
-                cout << "The column inserted is not correct!\n";
+                std::cout << "The column inserted is not correct!\n";
             }
-            else if (state[0][choice - 1] == 'X' || state[0][choice - 1] == 'O') {
-                cout << "That column is full\n";
+            else if (state[0][choice - 1] == ID1 || state[0][choice - 1] == ID2) {
+                std::cout << "That column is full\n";
             }
-            cout << "Please enter a number between 1 and " << length << ": ";
-            cin  >> choice;
+            std::cout << "Please enter a number between 1 and " << length << ": ";
+            std::cin  >> choice;
         }
         return choice;
     }
     void updateBoard(int choice, Player activePlayer) {
         for (int i = height - 1; i >= 0; i--)
-            if (state[i][choice] != 'X' && state[i][choice] != 'O') {
+            if (state[i][choice] != ID1 && state[i][choice] != ID2) {
                 state[i][choice] = activePlayer.getPlayerID();
                 break;
             }
@@ -162,11 +155,11 @@ public:
     void reset() {
         for (int i = 0; i < height; i++)
             for (int j = 0; j < length; j++)
-                state[i][j] = '#';
+                state[i][j] = EMPTY_CELL;
     }
     int isFull() {
         for (int j = 0; j < length; j++)
-            if (state[0][j] == '#')
+            if (state[0][j] == EMPTY_CELL)
                 return 0;
         return 1;
     }
@@ -181,8 +174,8 @@ private:
 public:
     Game() {
         board = Board();
-        playerOne = Player('X');
-        playerTwo = Player('O');
+        playerOne = Player(ID1);
+        playerTwo = Player(ID2);
     }
     Game(Board newBoard, Player newPlayer1, Player newPlayer2) {
         board = newBoard;
@@ -190,23 +183,23 @@ public:
         playerTwo = newPlayer2;
     }
     void gameWin(Player activePlayer){
-        cout << "\n" << activePlayer.getPlayerName() << " Connected Four, You Win!" << "\n";
+        std::cout << "\n" << activePlayer.getPlayerName() << " Connected Four, You Win!" << "\n";
     }
     void gameDraw() {
-        cout << "\nThe board is full, it's a draw!" << "\n";
+        std::cout << "\nThe board is full, it's a draw!" << "\n";
     }
 
     void startGame() {
-        cout << board << "\n";
+        std::cout << board << "\n";
         int again = 0;
         while (again != 2) {
             int choice = board.makeMove(playerOne);
             if (choice != -1) {
                 board.updateBoard(choice - 1, playerOne);
-                cout << board << "\n";
+                std::cout << board << "\n";
             }
             else {
-                cout << "Too many wrong tries!\n";
+                std::cout << "Too many wrong tries!\n";
                 break;
             }
 
@@ -223,7 +216,7 @@ public:
             choice = board.makeMove(playerTwo);
             board.updateBoard(choice - 1, playerTwo);
 
-            cout << board << "\n";
+            std::cout << board << "\n";
             if (board.checkFour(playerTwo))
             {
                 gameWin(playerTwo);
@@ -242,37 +235,37 @@ public:
     }
     int restartGame() {
         int restart;
-        cout << "Would you like to play again? Yes(1) No(2): ";
-        cin  >> restart;
+        std::cout << "Would you like to play again? Yes(1) No(2): ";
+        std::cin  >> restart;
         if (restart == 1){
             board.reset();
             return restart;
         }
-        else cout << "Thanks for playing!" << "\n";
+        else {
+            std::cout << "Thanks for playing!" << "\n";
+        }
         return 2;
     }
     ~Game() {
-        cout << "Game over!\n";
+        std::cout << "Game over!\n";
     }
 };
 
 
 int main()
 {
-	Player playerOne, playerTwo;
+    std::string namePlayerOne, namePlayerTwo;
+	std::cout << "Let's Play Connect 4" << "\n\n";
+	std::cout << "Player One please enter your name: ";
+	std::cin >> namePlayerOne;
+	std::cout << "Player Two please enter your name: ";
+	std::cin >> namePlayerTwo;
+
 	Board board;
-    string namePlayerOne, namePlayerTwo;
-	cout << "Let's Play Connect 4" << "\n\n";
-	cout << "Player One please enter your name: ";
-	cin  >> namePlayerOne;
-	playerOne.setPlayerName(namePlayerOne);
-	playerOne.setPlayerID('X');
-	cout << "Player Two please enter your name: ";
-	cin  >> namePlayerTwo;
-	playerTwo.setPlayerName(namePlayerTwo);
-	playerTwo.setPlayerID('O');
+	Player playerOne(namePlayerOne, ID1), playerTwo(namePlayerTwo, ID2);
 	Game game(board, playerOne, playerTwo);
-	cout << board << "\n";
+
+	std::cout << board << "\n";
 	game.startGame();
     return 0;
 }
